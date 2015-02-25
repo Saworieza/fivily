89b1b25f9ae8331ab5284d4bbb526b6ed9023067
yuejn <jennifer8ho@gmail.com>
2015-02-25 11:02:09 +0800

    changed to fivily

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  config.action_mailer.default_url_options = { host: 'enigmatic-citadel-5406.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'fivily.herokuapp.com' }
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
